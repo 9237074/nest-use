e2ffda75cda451bed3657e293aebbe693276c693
9237074 <9237074@qq.com>
2022-08-12 01:38:54 +0800

    fix: tsc tsconfig

--- src/log/default.log.ts
+++ src/log/default.log.ts
@@ -2,7 +2,7 @@ import { ConsoleLogger } from '@nestjs/common';
 
 export class DefaultLogger extends ConsoleLogger {
   private readonly debugEnv = ['develop', 'local'];
-  private readonly isDebug = this.debugEnv.includes(process.env.NODE_ENV);
+  private readonly isDebug = this.debugEnv.includes(process.env.NODE_ENV || '');
   debug(message: any, context?: string): void {
     if (this.isDebug) super.debug(message, context);
   }
